51632f36e919bd0c780ccba19705c264cbbdc83b
Nargiza Sainazarova <nargizasaina@gmail.com>
2023-01-05 13:34:36 +0600

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const neededNumber = n.toString().split('');
+  if(neededNumber[0] === '-') {
+      let preResult = neededNumber.slice(1);
+      return preResult.reverse().join('');
+  }
+  return neededNumber.reverse().join('');
 }
